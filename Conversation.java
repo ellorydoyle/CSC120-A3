11beb759279e6d8d9792c65e13fda037cbad55f7
Ellory <edoyle@smith.edu>
2023-02-17 15:11:55 -0500

    Final Versions, I believe!

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,22 @@
+/**
+ * Just letting you know, I had issues with the scanner. The scanner kept reading an input that didn't exist before the user gets to input anything.
+ * To combat this, I increase the round count by one and clear any possible responses that may come before the user gets to type a response.
+ * I also prevent those wrongly scanned blanks from appearing on the transcript.
+ * I'm sorry if this impacts the grading process negatively
+ */
+
+/**
+ *  Filename: Conversation.java
+    Description: A Chatbot!
+    Author: Ellory Doyle (@ellorydoyle)
+    Date: 17 February 2023
+ */
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
+/**
+ * The conversation class, everything happens in here.
+ */
 class Conversation {
   public static void main(String[] arguments) {
     // you will start the conversation here.
@@ -24,13 +40,11 @@ class Conversation {
     System.out.println("Hey there! What are you thinking about?"); 
     for (int i = 0; i < rounds; i++){
       userResponse = userinput.nextLine();
-      transcript.add(userResponse);
-      //Adds a space at the beginning so that pronouns at the beginning are recognized
+      //Preventing the transcript from adding the improperly scanned inputs
+      if (i>0){transcript.add(userResponse);}
       String[] words = userResponse.split(" ");
       for (int j=0; j<words.length; j++){
-          //Some of the replacements are modified because those replacements would be incorrectly replaced otherwise
         if (words[j].equalsIgnoreCase("I")) {
-          //Some of the replacements are modified because those replacements would be incorrectly replaced otherwise
           words[j]="you";
         }
         else if (words[j].equalsIgnoreCase("me")) {
@@ -62,6 +76,7 @@ class Conversation {
             int index = random_method.nextInt(cannedResponses.size());
             compResponse = cannedResponses.get(index);
         }}
+        //Preventing the terminal from printing a response in the first round because the scanner is not working correctly
         if (i==0){
           compResponse = "";
         }
@@ -78,10 +93,13 @@ class Conversation {
         compResponse = compResponse.substring(0,1).toUpperCase()+compResponse.substring(1);
       }
       System.out.println(compResponse);
-      transcript.add(compResponse);
+      //Preventing the transcript from adding the improperly scanned inputs
+      if(i>0){transcript.add(compResponse);}
       compResponse = "";
     }
     System.out.println("Goodbye!");
+    transcript.add("Goodbye!");
+    System.out.println(transcript);
   userinput.close();
   }
 }
