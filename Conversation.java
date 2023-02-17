c1d61930f0a54ad55499da43db44c83d190eec4d
Ellory <edoyle@smith.edu>
2023-02-17 14:47:43 -0500

    This is my final without Javadoc.

--- Conversation.java
+++ Conversation.java
@@ -1,54 +1,89 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 class Conversation {
   public static void main(String[] arguments) {
     // you will start the conversation here.
-    int rounds = 0;
-    int count = 0;
-    String userResponse = "";
-    String compResponse = "";
-    String i = " i ";
-    String me = " me ";
-    String am = " am ";
-    String are = " are ";
-    String you = " you ";
-    String my = " my ";
-    String your = " your ";
-    String yours = " yours ";
-    String mine = " mine ";
+    String userResponse="";
+    String compResponse="";
+    ArrayList<String> cannedResponses = new ArrayList<String>();
+    cannedResponses.add("Mhm");
+    cannedResponses.add("Why don't you tell me more");
+    cannedResponses.add("Can you tell me more");
   
     Scanner userinput = new Scanner(System.in);
-
-    while (rounds <= 0) {
-      System.out.println("How many rounds of conversation do you want to have?");
-      rounds = userinput.nextInt();
-        if (rounds <= 0) {
-          System.out.println("Sorry, please input a valid number.");
-        }
-    }
-    System.out.println("Understood.\n");
-
+    System.out.println("How many rounds of conversation do you want to have?");
+    int rounds = userinput.nextInt();
+    rounds = rounds+1;
+      if (rounds <= 0) {
+        System.out.println("Sorry, please input a valid number.");
+      }
+    ArrayList<String> transcript = new ArrayList<String>();
+    System.out.println("Understood.");
+    transcript.add("Hey there! What are you thinking about?");
     System.out.println("Hey there! What are you thinking about?"); 
-    while (count <= rounds) {
+    for (int i = 0; i < rounds; i++){
       userResponse = userinput.nextLine();
+      transcript.add(userResponse);
       //Adds a space at the beginning so that pronouns at the beginning are recognized
-      compResponse = " "+userResponse;
-      //Some of the replacements are modified because those replacements would be incorrectly replaced otherwise
-      compResponse = compResponse.toLowerCase().replaceAll(i, "youx").replaceAll(me, "youx").replaceAll(am, are).replaceAll(you, "ix").replaceAll(my, "yourx").replaceAll(your, my).replaceAll(mine, "yoursx").replaceAll(yours, mine);
-      //Below is included to replace the modified replacements
-      compResponse = compResponse.replaceAll("youx",you).replaceAll("ix"," I ").replaceAll("yourx",your).replaceAll("yoursx",yours);
-      //Replaces any punctuation
-      compResponse = compResponse.replace(".","?").replace("!","?");
-      //Take out any white space at the start and end
-      compResponse = compResponse.trim();
+      String[] words = userResponse.split(" ");
+      for (int j=0; j<words.length; j++){
+          //Some of the replacements are modified because those replacements would be incorrectly replaced otherwise
+        if (words[j].equalsIgnoreCase("I")) {
+          //Some of the replacements are modified because those replacements would be incorrectly replaced otherwise
+          words[j]="you";
+        }
+        else if (words[j].equalsIgnoreCase("me")) {
+          words[j]="you";
+        }
+        else if (words[j].equalsIgnoreCase("am")) {
+          words[j]="are";
+        }
+        else if (words[j].equalsIgnoreCase("are")) {
+          words[j]="am";
+        }
+        else if (words[j].equalsIgnoreCase("you")) {
+          words[j]="I";;
+        }
+        else if (words[j].equalsIgnoreCase("my")) {
+          words[j]="your";
+        }
+        else if (words[j].equalsIgnoreCase("your")) {
+          words[j]="my";
+        }
+        else{
+          words[j]=words[j];
+        }
+        compResponse = compResponse+" "+ words[j];
+        compResponse = compResponse.trim();
+        if (compResponse.equals(userResponse)){
+          Random random_method = new Random();
+          for (int k = 0; k < cannedResponses.size(); k++) {
+            int index = random_method.nextInt(cannedResponses.size());
+            compResponse = cannedResponses.get(index);
+        }}
+        if (i==0){
+          compResponse = "";
+        }
+      }
+      if (compResponse.contains(".")){
+        compResponse = compResponse.replace(".","");
+      }
+      if (compResponse.contains("!")){
+        compResponse = compResponse.replace("!","");}
+      if (!compResponse.contains("?")&&(!compResponse.isEmpty())){
+        compResponse = compResponse+"?";
+      }
+      if (!compResponse.isEmpty()){
+        compResponse = compResponse.substring(0,1).toUpperCase()+compResponse.substring(1);
+      }
       System.out.println(compResponse);
-      count += 1;
+      transcript.add(compResponse);
+      compResponse = "";
     }
-    
-    userinput.close();
-
-///delete spaces at the beginning and end
-///if punctuation at the end, replace with "?"
-///if no punctuation at end, add "?"
-///if sentence starts with one of the pronouns, recognize it even though it doesn't start with a space
+    System.out.println("Goodbye!");
+  userinput.close();
   }
 }
+
+
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    Yaya Callahan (At the 2/15 TA), Ashby
 - Which session(s) of TA / office hours did you attend?
+   TA (2/15 7-9 PM), Office hours (2/17)
 - What are your initial impressions of Java? 
+    Much easier to use than Python in terms of OO work, but I like Python much better in general
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    I feel like my brain works better in a procedural fashion than OOP, but I appreciate how contained Java can be. Now that I'm working in Java, I recognize the common techniques for looping moreso, especially through talking with Yaya, who has a much more streamlined code
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    I didn't know that I should iterate through each word, but now that I know, it makes much more sense! I would tell someone to go to office hours asap!
